869447cbb14c47c4d8dc4dee401265653b36d66c
Adam Tokarski <a.tokarski126@gmail.com>
2022-08-28 20:42:13 +0100

    add func to add random numbers

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-# import random
+import random
 import os
+# TODO make branches
 
 
 class Gra:
+    possible_el = [2, 4]
+    el_probability = [0.9, 0.1]
     board = []
+    size = 0
 
     def __init__(self, size=4):
+        self.size = size
         for row in range(size):
             self.board.append([])
             for column in range(size):
                 self.board[row].append(0)
 
     def print_board(self):
+        # TODO add formatting to print board pretty good even if there are longer numbers
         for row in self.board:
             for column in row:
                 print(column, end=" ")
@@ -25,6 +31,36 @@ class Gra:
         """
         return any(0 in el for el in self.board)
 
+    def add_element(self):
+        el = random.choices(self.possible_el, self.el_probability)[0]
+        while True:
+            y_coordinate = random.randint(0, self.size - 1)
+            x_coordinate = random.randint(0, self.size - 1)
+            if self.board[y_coordinate][x_coordinate] == 0:
+                self.board[y_coordinate][x_coordinate] = el
+                break
+
+    def move_left(self):
+        dodano = False
+        for row in self.board:
+            for i, el in enumerate(row):
+                dest = i
+                if i == 0:
+                    continue
+                for j in range(i, -1, -1):
+                    if row[j] == 0:
+                        dest = j
+                row[dest] = el
+                if dest != i:
+                    row[i] = 0
+
+                if dodano:
+                    dodano = False
+                    continue
+                if row[dest - 1] == row[dest]:
+                    row[dest - 1] *= 2
+                    row[dest] = 0
+                    dodano = True
 
 def clear_console():
     os.system("cls")
@@ -49,39 +85,47 @@ def move_up(board):
         print(dest_row, end=" ")
     return board
 
-
+"""
 # plansza = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 board = [[1, 2, 3, 4],
          [0, 5, 0, 8],
          [11, 0, 6, 10],
          [0, 0, 0, 7]]
 
+def ruch_w_lewo(tablica):
+    dodano = False
+    for i, el in enumerate(tablica):
+        dest = i
+        if i == 0:
+            continue
+        for j in range(i, -1, -1):
+            if tablica[j] == 0:
+                dest = j
+        tablica[dest] = el
+        if dest != i:
+            tablica[i] = 0
 
-# print_board(board)
-# print()
-# move_up(board)
+        if dodano:
+            dodano = False
+            continue
+        if tablica[dest - 1] == tablica[dest]:
+            tablica[dest - 1] *= 2
+            tablica[dest] = 0
+            dodano = True
 
-tablica = [2, 2, 4, 4]
+tablica = [2, 2, 2, 2]
 # ruch w lewo
-print(f"input:  {tablica}")
-dodano = False
-for i, el in enumerate(tablica):
-    dest = i
-    if i == 0:
-        continue
-    for j in range(i, -1, -1):
-        if tablica[j] == 0:
-            dest = j
-    tablica[dest] = el
-    if dest != i:
-        tablica[i] = 0
-
-    if dodano:
-        dodano = False
-        continue
-    if tablica[dest - 1] == tablica[dest]:
-        tablica[dest - 1] *= 2
-        tablica[dest] = 0
-        dodano = True
-
-print(f"output: {tablica}")
+print(f"input:   {tablica}")
+ruch_w_lewo(tablica)
+print(f"output:  {tablica}")
+ruch_w_lewo(tablica)
+print(f"output2: {tablica}")
+"""
+x = Gra()
+for i in range(10):
+    x.add_element()
+    print()
+x.print_board()
+x.move_left()
+print()
+x.print_board()
