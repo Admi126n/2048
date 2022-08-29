929ba89a187db31f7a8af07f250d44a91fa5e993
Adam Tokarski <a.tokarski126@gmail.com>
2022-08-29 19:10:19 +0100

    clean code and make move left right funcs

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import random
 import os
-# TODO make branches
 
 
-class Gra:
+def move_horizontal(row):
+    added = False
+    for i, el in enumerate(row):
+        dest = i
+        if i == 0:
+            continue
+        for j in range(i, -1, -1):
+            if row[j] == 0:
+                dest = j
+        row[dest] = el
+        if dest != i:
+            row[i] = 0
+        if added:
+            added = False
+            continue
+        if row[dest - 1] == row[dest]:
+            row[dest - 1] *= 2
+            row[dest] = 0
+            added = True
+    return row
+
+
+class Game2048:
     possible_el = [2, 4]
     el_probability = [0.9, 0.1]
     score = 0
@@ -34,7 +55,7 @@ class Gra:
 
     def add_element(self):
         el = random.choices(self.possible_el, self.el_probability)[0]
-        while True:
+        while True and self.possible_move():
             y_coordinate = random.randint(0, self.size - 1)
             x_coordinate = random.randint(0, self.size - 1)
             if self.board[y_coordinate][x_coordinate] == 0:
@@ -44,27 +65,20 @@ class Gra:
     def get_actual_score(self):
         self.score = max(max(el) for el in self.board)
 
-    def move_left(self):
-        dodano = False
-        for row in self.board:
-            for i, el in enumerate(row):
-                dest = i
-                if i == 0:
-                    continue
-                for j in range(i, -1, -1):
-                    if row[j] == 0:
-                        dest = j
-                row[dest] = el
-                if dest != i:
-                    row[i] = 0
+    def move_right(self):
+        # TODO same as in move_left func
+        for k, row in enumerate(self.board):
+            row.reverse()
+            move_horizontal(row)
+            row.reverse()
+            self.board[k] = row
 
-                if dodano:
-                    dodano = False
-                    continue
-                if row[dest - 1] == row[dest]:
-                    row[dest - 1] *= 2
-                    row[dest] = 0
-                    dodano = True
+    def move_left(self):
+        # TODO something doesnt work, eg. 0222 returns 2400 instead of 4200, it doesnt work with bigger boards
+        for k, row in enumerate(self.board):
+            move_horizontal(row)
+            self.board[k] = row
+
 
 def clear_console():
     os.system("cls")
@@ -75,70 +89,3 @@ def print_board(board):
         for column in row:
             print(column, end=" ")
         print()
-
-
-def move_up(board):
-    for i, row in enumerate(board):
-        if i == 0:
-            continue
-        dest_row = 0
-        for j, el in enumerate(row):
-            for k in range(i, -1, -1):
-                if board[k][j] == 0:
-                    dest_row = k
-        print(dest_row, end=" ")
-    return board
-
-"""
-# plansza = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-board = [[1, 2, 3, 4],
-         [0, 5, 0, 8],
-         [11, 0, 6, 10],
-         [0, 0, 0, 7]]
-"""
-def ruch_w_lewo(tablica):
-    dodano = False
-    for i, el in enumerate(tablica):
-        dest = i
-        if i == 0:
-            continue
-        for j in range(i, -1, -1):
-            if tablica[j] == 0:
-                dest = j
-        tablica[dest] = el
-        if dest != i:
-            tablica[i] = 0
-
-        if dodano:
-            dodano = False
-            continue
-        if tablica[dest - 1] == tablica[dest]:
-            tablica[dest - 1] *= 2
-            tablica[dest] = 0
-            dodano = True
-
-tablica = [2, 2, 2, 2]
-# ruch w lewo
-print(f"input:   {tablica}")
-tablica.reverse()
-ruch_w_lewo(tablica)
-tablica.reverse()
-print(f"output:  {tablica}")
-
-# ruch w prawo
-# instead of making another func to move right we can reverse array, use move left func and reverse array once again
-tablica.reverse()
-ruch_w_lewo(tablica)
-tablica.reverse()
-
-print(f"output2: {tablica}")
-"""
-x = Gra()
-for i in range(10):
-    x.add_element()
-    print()
-x.print_board()
-x.move_left()
-print()
-x.print_board()
-"""
